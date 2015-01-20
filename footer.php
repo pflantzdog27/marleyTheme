411ee52e43807563a8d741b47ae8877341bb8058
Adam Pflantzer <apflantzer@gmail.com>
2015-01-20 14:28:50 -0500

    final revisions

---
+++ footer.php
@@ -0,0 +1,128 @@
+<footer id="footer" class="clearfix">
+        <div class="container">
+            <div class="row">
+                <div id="footer-main-wrap" class="col-sm-9">
+                    <nav id="footer-nav">
+                        <ul class="col-sm-3">
+                            <h4>Marley Family</h4>
+                            <li><a target="_blank" href="http://bobmarley.com">BobMarley.com</a></li>
+                            <li><a target="_blank" href="http://tuffgongworldwide.com">Tuff Gong Worldwide</a></li>
+                            <li><a target="_blank" href="http://houseofmarley.com">House of Marley</a></li>
+                            <li><a target="_blank" href="http://drinkmarley.com">Marley Beverage<br>Company</a></li>
+                            <li><a target="_blank" href="http://tuffgong.com">Tuff Gong International</a></li>
+                        </ul>
+                        <ul class="col-sm-3">
+                            <h4>Partners</h4>
+                            <li><a target="_blank" href="http://www.realcup.com/brands/#4_US-EN">RealCup™ Single Serve Cups</a></li>
+                            <li><a target="_blank" href="http://marleycoffeestores.com">AVT Automated Retail</a></li>
+                            <li><a target="_blank" href="http://www.gardenoflife.com/Products-for-Life/RAW-Fit.aspx">Garden of Life</a></li>
+							<li><a target="_blank" href="http://marleycoffee.com/corporate-sponsorships/">Sports Partnerships</a></li>
+                            <h4>International Partners</h4>
+                            <li><a target="_blank" href="http://www.marleycoffee.co.uk">United Kingdom</a></li>
+                            <li><a target="_blank" href="http://www.marleycoffee.cl">Chile</a></li>
+                            <li>Korea</li>
+                        </ul>
+                        <ul class="col-sm-3">
+                            <h4>Investor Relations</h4>
+                            <li><a href="http://investor.marleycoffee.com">Corporate Profile</a></li>
+                            <li><a href="http://investor.marleycoffee.com/team.php">Directors and Officers</a></li>
+                            <li><a href="http://investor.marleycoffee.com/news.php">News Releases</a></li>
+                            <li><a href="http://investor.marleycoffee.com/media-placements.php">Media Placements</a></li>
+                            <li><a href="http://investor.marleycoffee.com/sec-filings.php">SEC Filings</a></li>
+                            <li><a href="http://investor.marleycoffee.com/events.php">Calendar of Events</a></li>
+                        </ul>
+                        <ul class="col-sm-3">
+                            <h4>Join our Mailing List</h4>
+                            <li>Sign up for the Marley Coffee news and updates</li>
+                            <li><form action="https://docs.google.com/forms/d/1E0_5ZzYZW6eDcx5XkegO76KQ3KHHmoxUR2oNqtQEMVM/formResponse" method="POST" target="hidden_iframe" id="email-form-signup" onsubmit="submitted = true">
+                                    <input class="col-sm-8 email-input" type="email" placeholder="EMAIL" required name="entry.2058090366">
+									<input type="hidden" name="entry.864996450" value="Footer">
+                                    <input class="email-submit col-sm-4" type="submit" name="submit" value="submit">
+                                    <input type="hidden" name="draftResponse" value="[]">
+                                    <input type="hidden" name="pageHistory" value="0">
+                                </form>
+                                <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;">
+                                </iframe>
+                                <h4 style="display: none" id="submit-confirmation">Thank you.</h4>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <aside id="coffee-cup" class="col-sm-3 hidden-xs">
+                    <img src="<?php bloginfo('template_url')?>/images/coffee-cup.svg" alt="Marley Coffee Cup">
+                </aside>
+            </div>
+            <footer class="copyright col-sm-9">
+                <div class="fb-like" data-href="http://marleycoffee.com" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div>
+                <a href="https://twitter.com/MarleyCoffee" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @marleycoffee</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                <br>
+                <small>Copyright 2014 Marley Coffee/Jammin Java Corp. All rights reserved.</small>
+            </footer>
+        </div>
+    </footer>
+</div><!-- close wrapper -->
+
+    <!-- modals-->
+  <div class="modal fade" id="opportunity-contact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h2>Rise and Grind<span class="reg-trade" style="font-size: 14px">™</span> &nbsp; with Marley
+                    Coffee</h2>
+                </div>
+                <div class="modal-body">
+                    <form class="form-inline" role="form" action="https://docs.google.com/forms/d/1cHBOeGernMW5a4AnifvhLR3N591tcqND70GNcLOZYdA/formResponse" method="POST" id="opps-form" target="hidden_iframe_opps" onsubmit="submitted = true">
+                        <div class="form-group col-sm-6">
+                            <label class="sr-only" for="entry_1956374557">Name</label>
+                            <input type="text" class="form-control" name="entry.1956374557" value="" id="entry_1956374557" placeholder="Name" required>
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <label class="sr-only" for="entry_1502153747">Email</label>
+                            <input type="email" class="form-control" name="entry.1502153747" value="" id="entry_1502153747" placeholder="Email" required>
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <label class="sr-only" for="entry_1179014994">Phone</label>
+                            <input type="phone" class="form-control" name="entry.1179014994" value="" id="entry_1179014994" placeholder="Phone" required>
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <label class="sr-only" for="entry_2037342537">Program</label>
+                            <select id="program-select" class="form-control" name="entry.2037342537">
+                                <option>Office Coffee Service</option>
+                                <option>Colleges/Universities</option>
+                                <option>Hotels</option>
+                                <option>Restaurants</option>
+                                <option>Cafes</option>
+                                <option>Government/Military</option>
+                                <option>Travel/Recreation</option>
+                                <option>International Distribution</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-sm-12">
+                            <label class="sr-only" for="entry.1342121893">Comments</label>
+                            <textarea rows="8" class="form-control" name="entry.1342121893" id="entry_1342121893" placeholder="Comments"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <input type="submit" name="submit" value="SUBMIT" class="submit marley-btn btn btn-primary btn-block">
+                            <input type="hidden" name="draftResponse" value="[]">
+                            <input type="hidden" name="pageHistory" value="0">
+                        </div>
+                    </form>
+                </div>
+                <iframe name="hidden_iframe_opps" id="hidden_iframe_opps" style="display:none;">
+                </iframe>
+                <h4 style="text-align: center; display: none; padding: 15px;" id="submit-confirmation-opps">THANK YOU
+                    for your interest in Marley Coffee.<br> We look forward to speaking with you soon.</h4><br>
+            </div>
+        </div>
+    </div>
+
+    <!-- add this plugin -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4d9b516371593d6d"></script>
+    <!-- branfolder script -->
+    <script type="text/javascript">var bf_url="marleycoffee";(function(){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src="//d2sdf28wg0skh3.cloudfront.net/bf.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();</script>
+    <!--<script type="text/javascript">var bf_url="marleycoffeeinternational";(function(){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src="//d2sdf28wg0skh3.cloudfront.net/bf.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();</script>-->
+    <?php wp_footer(); ?>
+</body>
+</html>
